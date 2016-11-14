5b9b5f25aa9c4f12d65ccd1dfff5c02c08b6d49c
arlt209 <nickarlt@gmail.com>
2016-11-14 15:27:43 -0800

    added more forms and buttons for the rest of the UI for the  math business logic,    wired submit event handler to the correct  form ID's

--- calculator_v1/js/scripts.js
+++ calculator_v1/js/scripts.js
@@ -20,14 +20,47 @@ var divide = function(number1, number2) {
 // Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function() {
+  // add
   $("form#add").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
 
     result = number1 + number2;
-    $("#output").text(result);
+    $("#addOutput").text(result);
   });
+
+  // subtract
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+
+    result = number1 - number2;
+    $("#SubOutput").text(result);
+  });
+
+  // divide
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+
+    result = number1 / number2;
+    $("#divideOutput").text(result);
+  });
+
+  // module / remainder
+  $("form#remainder").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#remainder1").val());
+    var number2 = parseInt($("#remainder2").val());
+
+    result = number1 % number2;
+    $("#remainderOutput").text(result);
+  });
+
+
 });
 
 
